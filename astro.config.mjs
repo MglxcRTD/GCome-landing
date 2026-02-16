f52d1b1054a86ac6146e8082311eb0add8556f7c
Miguel <miguelroadtodam@gmail.com>
2026-02-16 16:27:27 +0100

    [feature]: add github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+
+  site: 'https://mglxcRTD.github.io',
+  base: '/GCome-landing',
   vite: {
     plugins: [tailwindcss()]
   }
